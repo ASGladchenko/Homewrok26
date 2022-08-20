76f10a9c7cedda41d1d87717d06ffe94f917413f
ASGladchenko <asgladchenko@gmail.com>
2022-08-20 18:15:23 +0300

    add card generator

--- js/main.js
+++ js/main.js
@@ -232,22 +232,55 @@ const users = [
 ];
 
 let renderUSer = (users) => {
-    console.log(users[0])
     users.forEach((item) => {
         let card = document.createElement("div")
+        card.classList = "col-12 col-md-6  px-4  bg-primary mb-3 p-0 border position-relative shadow"
         let userNickName = document.createElement("h3")
-        let userName = document.createElement("h3")
+        userNickName.classList = "text-center  text-white mb-2"
+        userNickName.innerHTML = item.username
+        let userName = document.createElement("h5")
+        userName.innerHTML = `Name : ` + item.name
         let address = document.createElement("p")
-        let email = document.createElement("p")
-
-
-            })
+        address.classList = "fz-12 mb-1"
+        address.innerHTML = `Address : ${item.address.street}, ${item.address.suite}, ${item.address.city}`
+        let zipCode = document.createElement('p')
+        zipCode.classList = " mb-1 fz-12"
+        zipCode.innerHTML = `Zip-code: ${item.address.zipcode}`
+        let phone = document.createElement("p")
+        phone.classList = "fz-12 mb-1"
+        let email = document.createElement("a")
+        email.innerHTML = `${item.email}`
+        email.classList = "text-white fz-12"
+        let eContact = document.createElement("p")
+        eContact.classList = "fz-12 mb-1"
+        eContact.textContent = "Email: "
+        let webSite = document.createElement('a')
+        webSite.innerHTML = item.website
+        webSite.classList = "text-white fz-12 mb-1"
+        let webSiteP = document.createElement('p')
+        webSiteP.textContent = "WebSite : "
+        webSiteP.classList = "fz-12 mb-1"
+        webSiteP.append(webSite)
+        eContact.append(email)
+        phone.innerHTML = `Phone number: : ${item.phone}`
+        let company = document.createElement("p")
+        company.innerHTML = `Company name : ${item.company.name} `
+        company.classList = "fz-12 mb-1"
+        let catchPhrase = document.createElement('p')
+        catchPhrase.innerHTML = `Cath Phrase : ${item.company.catchPhrase}`
+        catchPhrase.classList = "fz-12 mb-1 "
+        let bs = document.createElement('p')
+        bs.innerHTML = `BS : ${item.company.bs}`
+        bs.classList = 'fz-12 mb-1'
+        let location = document.createElement("p")
+        location.innerHTML = `GEO Location:  lat: ${item.address.geo.lat} lng: ${item.address.geo.lng}`
+        location.classList = "fz-12 text-right mb-1 "
+        let idUSer = document.createElement('p')
+        idUSer.innerHTML = item.id
+        idUSer.classList = "mb-0 position-absolute idUser"
+        card.append(userNickName, userName, address, phone, zipCode, eContact, webSiteP, company, catchPhrase, bs, location,idUSer)
+        document.getElementsByClassName(`cards`)[0].append(card)
+    })
 }
-let x = document.createElement("div")
-let email = document.createElement("p")
-email.innerHTML = "What's up man"
-email.classList = "m-auto"
-x.append(email)
 
-document.body.append(x)
-// renderUSer(users);
+renderUSer(users);
